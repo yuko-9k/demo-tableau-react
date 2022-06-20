3ff78d7d9cf1f5aca10b923337ca1130e61773f8
Dang Nguyen <dtn1712.work@gmail.com>
2022-06-20 19:03:07 +0700

    implement tableau-react package

---
+++ pages/component/Tableau.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import { isMobile, isTablet } from "react-device-detect";
+
+let TableauReport;
+
+const Tableau = () => {
+  const [screenWidth, setScreenWidth] = useState(0);
+
+  const options = {
+    padding: "0",
+    margin: "0",
+    width: `${screenWidth}px`,
+    height: "2227px",
+    device: isMobile ? "phone" : isTablet ? "tablet" : "desktop",
+  };
+
+  useEffect(() => {
+    if (typeof window !== undefined) {
+      TableauReport = require("tableau-react");
+      setScreenWidth(window.innerWidth);
+    }
+  }, []);
+  return (
+    <>
+      {screenWidth ? (
+        <TableauReport
+          url="https://public.tableau.com/views/LeadershipDraft/AssetsDashboard?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link"
+          options={options}
+        />
+      ) : (
+        <div>loading</div>
+      )}
+    </>
+  );
+};
+
+export default Tableau;
